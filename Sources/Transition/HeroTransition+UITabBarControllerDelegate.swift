abc6a65d0ccf16ce1d917c98990448dfa2aab0be
johankasperi <johan@kasperi.se>
2017-12-17 05:04:30 +0100

    Create HeroTabBarControllerDelegate protocol. Implement some common UITabBarControllerDelegate methods.

--- Sources/Transition/HeroTransition+UITabBarControllerDelegate.swift
+++ Sources/Transition/HeroTransition+UITabBarControllerDelegate.swift
@@ -22,18 +22,35 @@
 
 import UIKit
 
+@objc public protocol HeroTabBarControllerDelegate {
+  @objc optional func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController)
+  @objc optional func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool
+  @objc optional func tabBarController(_ tabBarController: UITabBarController, willBeginCustomizing viewControllers: [UIViewController])
+}
+
 extension HeroTransition: UITabBarControllerDelegate {
+  
+  public func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+    if let delegate = tabBarController as? HeroTabBarControllerDelegate {
+      delegate.tabBarController?(tabBarController, didSelect: viewController)
+    }
+  }
+  
   public func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
     if isTransitioning {
       cancel(animate: false)
     }
+    if let delegate = tabBarController as? HeroTabBarControllerDelegate,
+      let response = delegate.tabBarController?(tabBarController, shouldSelect: viewController) {
+      return response
+    }
     return true
   }
-
+  
   public func tabBarController(_ tabBarController: UITabBarController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
     return interactiveTransitioning
   }
-
+  
   public func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
     guard !isTransitioning else { return nil }
     self.state = .notified
@@ -44,5 +61,11 @@ extension HeroTransition: UITabBarControllerDelegate {
     self.toViewController = toViewController ?? toVC
     self.inTabBarController = true
     return self
+  }
+  
+  public func tabBarController(_ tabBarController: UITabBarController, willBeginCustomizing viewControllers: [UIViewController]) {
+    if let delegate = tabBarController as? HeroTabBarControllerDelegate {
+      delegate.tabBarController?(tabBarController, willBeginCustomizing: viewControllers)
+    }
   }
 }
